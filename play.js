09cec8726ef97c28e2acde8124e20f0570db3e66
xoos0420 <xoos0420@gmail.com>
2023-12-20 17:48:55 +0900

    node_231220_1748

--- play.js
+++ play.js
@@ -1,4 +1,27 @@
-const hobbies = ['Sports', 'Cooking'];
-const copiedArr =[hobbies];
+const person = {
+    name: 'Max',
+    age: 29,
+    greet() {
+        console.log('Hi, I am ' + this.name)
+    }
+};
 
-console.log(copiedArr);
+const printName = ({ name }) => {
+    console.log(name);
+}
+
+printName(person);
+
+// const copiedPerson = {...person};
+// console.log(copiedPerson);
+
+// const hobbies = ['Sports', 'Cooking'];
+// const copiedArr =[...hobbies];
+
+// console.log(copiedArr);
+
+// const toArr = (...args) => {
+//     return args;
+// };
+
+// console.log(toArr(1, 2, 3, 4));
