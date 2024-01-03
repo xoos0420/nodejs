bb2f526c0e45cf1de2aecce7e4ba7ee88e89baf4
xoos0420 <xoos0420@gmail.com>
2024-01-03 17:45:00 +0900

    node modules

---
+++ app.js
@@ -0,0 +1,12 @@
+// core module
+const http = require('http');
+
+// 사용자 지정 파일
+const routes = require('./routes')
+
+console.log(routes.someText);
+
+// 서버 생성
+const server = http.createServer(routes.handler);
+
+server.listen(3000);
