af706ccf95fd54391401ba558b62e43f14524eb0
xoos <xoos0420@gmail.com>
2024-03-29 19:21:42 +0900

    create template & layout

--- app.js
+++ app.js
@@ -6,12 +6,7 @@ const expressHbs = require('express-handlebars')
 
 const app = express();
 
-app.engine('hbs', expressHbs({
-    layoutsDir: 'views/layouts/',
-    defaultLayout: 'main-layout',
-    extname: 'hbs',
-}));
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin.js')
@@ -24,7 +19,10 @@ app.use('/admin', adminData.routes);
 app.use(shopRouter);
 
 app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: 'Error'})
+    res.status(404).render('404', {
+        pageTitle: 'Error',
+        path: 'Error'
+    })
 })
 
 app.listen(3000);
