9a811ff9411ac5b84c2a44e7a64e1564fde3187e
xoos <xoos0420@gmail.com>
2024-03-31 23:43:02 +0900

    Split more pages and also routers, controllers

--- controllers/error.js
+++ controllers/error.js
@@ -1,6 +1,6 @@
 exports.gett404 = (req, res, next) => {
     res.status(404).render('404', {
         pageTitle: 'Error',
-        path: 'Error'
+        path: '/404'
     })
 }
--- routes/shop.js
+++ routes/shop.js
@@ -2,10 +2,16 @@ const path = require('path');
 
 const express = require('express')
 
-const productsController = require('../controllers/products.js')
+const shopController = require('../controllers/shop.js')
 
 const router = express.Router();
 
-router.get('/', productsController.getProducts);
+router.get('/', shopController.getIndex);
+
+router.get('/products', shopController.getProducts);
+
+router.get('/cart', shopController.getCart);
+
+router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
