a196d97de19845de4a18dc7d7ec69347dcf7c9b4
xoos <xoos0420@gmail.com>
2024-04-02 20:56:20 +0900

    Passing data with a POST req

--- controllers/shop.js
+++ controllers/shop.js
@@ -10,6 +10,18 @@ exports.getProducts = (req, res, next) => {
     });
 }
 
+exports.getProduct = (req, res, next) => {
+    const prodId = req.params.productId;
+    Product.findById(prodId, product => {
+        console.log(product)
+        res.render('shop/product-detail', {
+            product: product,
+            pageTitle: product.title,
+            path:'/products'
+        });
+    });
+}
+
 exports.getIndex = (req, res, next) => {
     const products = Product.fetchAll((products) => {
         res.render('shop/index', {
@@ -26,6 +38,12 @@ exports.getCart = (req, res, next) => {
         pageTitle: 'Your Cart',
     });
 }
+
+exports.postCart = (req, res, next) => {
+    const prodId = req.body.productId;
+    console.log(prodId);
+    res.redirect('/cart');
+};
 
 exports.getOrders = (req, res, next) => {
     res.render('shop/orders', {
--- routes/shop.js
+++ routes/shop.js
@@ -10,7 +10,12 @@ router.get('/', shopController.getIndex);
 
 router.get('/products', shopController.getProducts);
 
+router.get('/products/:productId', shopController.getProduct);
+// 콜론으로 동적 매개변수를 설정할 수 있음
+
 router.get('/cart', shopController.getCart);
+
+router.post('/cart', shopController.postCart);
 
 router.get('/orders', shopController.getOrders);
 
